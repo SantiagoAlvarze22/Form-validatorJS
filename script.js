5717e6eee7b360f8243ad7ad0f99b5d7479a165f
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-07 13:22:38 -0500

    check length, email and passwords matchÂ

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const username = document.getElementById('username')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const password2 = document.getElementById('password2')
-const inputs = document.querySelectorAll('.input')
 
 //show input error message
 function showError(input, message){
@@ -22,36 +21,57 @@ function showSucess(input){
 }
 
 //check email is valid 
-function isValidEmail(email){
+function checkEmail(input){
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if(re.test(input.value.trim())){
+    showSucess(input)
+  }else{
+    showError(input, 'Email is not valid')
+  }
+}
+
+//check input lenght
+function checkLength(input, min, max){
+  if(input.value.length <min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+  }else if(input.value.length > max){
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+  }else{
+    showSucess(input)
+  }
+}
+
+//check if the passwords match
+function checkPasswordsMatch(input1, input2){
+  if(input1.value !== input2.value){
+    showError(input2, 'Passwords do not match')
+  }
+}
+
+//get fieldName
+function getFieldName(input){
+  //taking the first letter and becoming uppercase then concatenating the second part of the word
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//check required fields
+function checkRequired(inputArr){
+  inputArr.forEach(input => {
+    if(input.value.trim() === ''){
+      showError(input, `${getFieldName(input)} is required`)
+    }else{
+      showSucess(input)
+    }
+  });
 }
 
 //event listeners
 form.addEventListener('submit', e => {
   e.preventDefault()
-  if(username.value=== ''){
-    showError(username, 'Username is required')
-  }else{
-    showSucess(username)
-  }
-  if(email.value=== ''){
-    showError(email, 'Email is required')
-  }else if(!isValidEmail(email.value)){
-    showError(email, 'Email is not valid')
-  }
-  else{
-    showSucess(email)
-  }
-  if(password.value=== ''){
-    showError(password, 'Password is required')
-  }else{
-    showSucess(password)
-  }
-  if(password2.value=== ''){
-    showError(password2, 'Password2 is required')
-  }else{
-    showSucess(password2)
-  }
   
+  checkRequired([username, email, password, password2])
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25)
+  checkEmail(email)
+  checkPasswordsMatch(password, password2)
 })
